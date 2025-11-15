b2e8677066b7c15424e5e639306e18e16284a999
Flynn <cirilla@tuta.io>
2025-11-15 12:08:37 +0000

    Update AppStore URL

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { onGe0Decode } from './ge0';
 
 const NOT_FOUND_REDIRECT_URL = 'https://comaps.app';
 const GE0_TEMPLATE_PATH = '/ge0.html';
-const APPSTORE = 'fixme';//TODO: Update'https://apps.apple.com/app/organic-maps/id1567437057';
+const APPSTORE = 'https://apps.apple.com/us/app/comaps/id6747180809';
 const GOOGLE = 'https://play.google.com/store/apps/details?id=app.comaps.google';
 const HUAWEI = 'fixme';//TODO: Update'https://appgallery.huawei.com/#/app/C104325611';
 const FDROID = 'https://f-droid.org/en/packages/app.comaps.fdroid/';
